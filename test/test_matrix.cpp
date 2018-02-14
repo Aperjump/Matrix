bef185923d993e3fbc4ddabe37f80554a38458e7
Aperjump <wangwei_aperion@163.com>
2018-02-14 14:25:48 -0800

    add test case

---
+++ test/test_matrix.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <CL/cl.h>
+#include <initializer_list>
+#include <ctime>
+#include <string>
+#include "matrix_expr.h"
+#include "matrix.h"
+#define FILE_NAME "kernel.cl"
+#define SIZE 5
+template<typename Expr>
+void print_all(Expr const & mat);
+int main()
+{
+	using namespace ww_matrix;
+	ww_clwrapper::_clconfig.init("gpu", 0, 0);
+	ww_clwrapper::_clconfig.create_program(FILE_NAME);
+	Matrix<double> a(SIZE, SIZE, 1);
+	Matrix<double> b(SIZE, SIZE, 2);
+	Matrix<double> c(SIZE, SIZE, 5);
+	Matrix<double> d = a + b;
+	print_all(d);
+	Matrix<double> e = b * c;
+	print_all(e);
+	Matrix<double> f = a + b + c * e *d;
+	print_all(f);
+	Matrix<double> g = (a + b)*c + d * f;
+	print_all(g);
+}
+/*
+3.00, 3.00, 3.00, 3.00, 3.00,
+3.00, 3.00, 3.00, 3.00, 3.00,
+3.00, 3.00, 3.00, 3.00, 3.00,
+3.00, 3.00, 3.00, 3.00, 3.00,
+3.00, 3.00, 3.00, 3.00, 3.00,
+
+50.00, 50.00, 50.00, 50.00, 50.00,
+50.00, 50.00, 50.00, 50.00, 50.00,
+50.00, 50.00, 50.00, 50.00, 50.00,
+50.00, 50.00, 50.00, 50.00, 50.00,
+50.00, 50.00, 50.00, 50.00, 50.00,
+
+18753.00, 18753.00, 18753.00, 18753.00, 18753.00,
+18753.00, 18753.00, 18753.00, 18753.00, 18753.00,
+18753.00, 18753.00, 18753.00, 18753.00, 18753.00,
+18753.00, 18753.00, 18753.00, 18753.00, 18753.00,
+18753.00, 18753.00, 18753.00, 18753.00, 18753.00,
+
+281370.00, 281370.00, 281370.00, 281370.00, 281370.00,
+281370.00, 281370.00, 281370.00, 281370.00, 281370.00,
+281370.00, 281370.00, 281370.00, 281370.00, 281370.00,
+281370.00, 281370.00, 281370.00, 281370.00, 281370.00,
+281370.00, 281370.00, 281370.00, 281370.00, 281370.00,
+*/
+template<typename Expr>
+void print_all(Expr const & mat) {
+	for (size_t i = 0; i < mat.rw_size(); i++) {
+		for (size_t j = 0; j < mat.cl_size(); j++) {
+			printf("%.2f, ", mat(i, j));
+		}
+		printf("\n");
+	}
+}
---
+++ test/test_simple_matrix.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <cstdlib>
+#include <CL/cl.h>
+#include <initializer_list>
+#include <ctime>
+#include <string>
+#include "matrix_expr.h"
+#define SIZE 3200
+#define FILE_NAME "kernel.cl"
+template<typename Expr>
+void print_all(Expr const & mat);
+int main()
+{
+	using namespace ww_simple_matrix;
+	SMatrix<double> a = { { 1,2,3 },{ 2,3,4 },{ 4,5,6 } };
+	SMatrix<double> b = { { 1,2,3 },{ 2,3,4 },{ 4,5,6 } };
+	SMatrix<double> c(3, 3, 2);
+	SMatrix<double> d(3, 3, 5);
+	SMatrix<double> e;
+	print_all(a);
+	printf("\n");
+	e = a + b;
+	print_all(e);
+	printf("\n");
+	e = a * b;
+	print_all(e);
+	printf("\n");
+	e = a + b + c + d;
+	print_all(e);	
+	printf("\n");
+	e = a * b * c * d;
+	print_all(e);
+	printf("\n");
+	SMatrix<double> f(3, 2, 7);
+	SMatrix<double> g(2, 5, 4);
+	SMatrix<double> h = f * g;
+	print_all(h);
+	return 0;
+}
+/*
+1.00, 2.00, 3.00,
+2.00, 3.00, 4.00,
+4.00, 5.00, 6.00,
+
+2.00, 4.00, 6.00,
+4.00, 6.00, 8.00,
+8.00, 10.00, 12.00,
+
+17.00, 23.00, 29.00,
+24.00, 33.00, 42.00,
+38.00, 53.00, 68.00,
+
+9.00, 11.00, 13.00,
+11.00, 13.00, 15.00,
+15.00, 17.00, 19.00,
+
+2070.00, 2070.00, 2070.00,
+2970.00, 2970.00, 2970.00,
+4770.00, 4770.00, 4770.00,
+
+56.00, 56.00, 56.00, 56.00, 56.00,
+56.00, 56.00, 56.00, 56.00, 56.00,
+56.00, 56.00, 56.00, 56.00, 56.00,
+
+*/
+template<typename Expr>
+void print_all(Expr const & mat) {
+	for (size_t i = 0; i < mat.rw_size(); i++) {
+		for (size_t j = 0; j < mat.cl_size(); j++) {
+			printf("%.2f, ", mat(i, j));
+		}
+		printf("\n");
+	}
+}
